cc1481973bd0db2a5cf91de8e62d6757b2a8ac31
Sami Airaksinen <sami.airaksinen@nitorcreations.com>
2015-03-18 08:24:13 +0200

    Refactor pipelines
    - long stream chains to multiple lines
    - extract mapping methods to own methods in the presenter class

--- drm-core/src/main/java/com/nitorcreations/presenters/DefaultGraphvizPresenter.java
+++ drm-core/src/main/java/com/nitorcreations/presenters/DefaultGraphvizPresenter.java
@@ -18,10 +18,6 @@ public class DefaultGraphvizPresenter implements Presenter {
     private static final String INHERITANCE_STYLE = "arrowhead=empty color=slategray";
     private final AtomicInteger count = new AtomicInteger();
 
-    private String describeEdge(Edge edge) {
-        return String.format("%s -> %s [%s];", edge.source.className, edge.target.className, getEdgeDescription(edge));
-    }
-
     private Object getEdgeDescription(Edge edge) {
         StringBuilder sb = new StringBuilder();
         if (edge.target.description != null) {
@@ -44,29 +40,64 @@ public class DefaultGraphvizPresenter implements Presenter {
         return "dir=both arrowhead=open arrowtail=open";
     }
 
-
     private String describeInheritance(List<Edge> edges) {
-        return edges.stream().filter(e -> e.type == EdgeType.EXTENDS).map(hierarchyEdge -> String.format("  %s -> %s [%s];\n", hierarchyEdge.source.className, hierarchyEdge.target.className, INHERITANCE_STYLE)).collect(joining());
+        return edges.stream()
+                .filter(e -> e.type == EdgeType.EXTENDS)
+                .map(this::describeInheritance)
+                .collect(joining());
+    }
+
+    private String describeInheritance(Edge hierarchyEdge) {
+        return String.format("  %s -> %s [%s];\n",
+                hierarchyEdge.source.className,
+                hierarchyEdge.target.className,
+                INHERITANCE_STYLE);
     }
 
     private String describePackages(List<DomainObject> domainObjects) {
         count.set(0);
-        return domainObjects.stream().collect(groupingBy(DomainObject::getPackageName)).entrySet().stream().map(this::describePackage).collect(joining());
-
+        return domainObjects.stream()
+                .collect(groupingBy(DomainObject::getPackageName))
+                .entrySet().stream()
+                .map(this::describePackage)
+                .collect(joining());
     }
 
     private String describePackage(Map.Entry<String, List<DomainObject>> entry) {
         return String.format("  subgraph cluster_%s {\n    label = \"%s\";\n%s  }\n",
-                count.getAndIncrement(), entry.getKey(), entry.getValue().stream().map(domainObject -> "    " + domainObject.className + "\n").distinct().collect(joining()));
+                count.getAndIncrement(),
+                entry.getKey(),
+                listDomainObjects(entry.getValue()));
+    }
+
+    private String listDomainObjects(List<DomainObject> domainObjects) {
+        return domainObjects.stream()
+                .map(domainObject -> "    " + domainObject.className + "\n")
+                .distinct()
+                .collect(joining());
     }
 
     private String describeCompositions(List<Edge> edges) {
-        return edges.stream().filter(e -> e.type != EdgeType.EXTENDS).map(e -> String.format("  %s\n", describeEdge(e))).collect(joining());
+        return edges.stream()
+                .filter(e -> e.type != EdgeType.EXTENDS)
+                .map(this::describeComposition)
+                .collect(joining());
     }
 
+    private String describeComposition(Edge compositionEdge) {
+        return String.format("  %s\n", describeEdge(compositionEdge));
+    }
+
+    private String describeEdge(Edge edge) {
+        return String.format("%s -> %s [%s];", edge.source.className, edge.target.className, getEdgeDescription(edge));
+    }
 
     @Override
     public String describe(List<DomainObject> domainObjects, List<Edge> edges) {
-        return DOMAIN_DECLARATION + DEFAULTS + "\n" + describePackages(domainObjects) + describeCompositions(edges) + describeInheritance(edges) + "}";
+        return DOMAIN_DECLARATION + DEFAULTS + "\n"
+                + describePackages(domainObjects)
+                + describeCompositions(edges)
+                + describeInheritance(edges)
+                + "}";
     }
 }
