3a2264691f512ee40e552729c739f26777fc6eba
Sami Airaksinen <sami.airaksinen@nitorcreations.com>
2015-04-14 16:34:02 +0300

    Filter anonymous class instances from the list of founded classes in package

--- drm-core/src/main/java/com/nitorcreations/DomainClassFinder.java
+++ drm-core/src/main/java/com/nitorcreations/DomainClassFinder.java
@@ -11,25 +11,26 @@ import java.util.stream.Collectors;
 
 class DomainClassFinder {
 
-    private static boolean shouldBeIncluded(Class<?> clazz) {
-        return !clazz.getSimpleName().equals("package-info");
-    }
-
     public static List<Class<?>> findClasses(final List<String> packages, final URLClassLoader classLoader) {
         return packages.stream()
                 .map(packageName -> getClasses(classLoader, packageName))
                 .flatMap(Collection::stream)
-                .filter(DomainClassFinder::shouldBeIncluded)
+                .filter(DomainClassFinder::isNotPackageInfo)
+                .filter(DomainClassFinder::isNotAnonymousClass)
                 .collect(Collectors.toList());
+    }
+
+    private static boolean isNotPackageInfo(Class<?> clazz) {
+        return !clazz.getSimpleName().equals("package-info");
+    }
+
+    private static boolean isNotAnonymousClass(Class<?> clazz) {
+        return !clazz.getSimpleName().equals("");
     }
 
     private static Set<Class<?>> getClasses(URLClassLoader classLoader, String packageName) {
         Reflections reflections = new Reflections(packageName, new SubTypesScanner(false), classLoader);
         return reflections.getSubTypesOf(Object.class);
-    }
-
-    public static List<Class<?>> findClasses(final List<String> packages) {
-        return findClasses(packages, null);
     }
 
     private DomainClassFinder() {
--- drm-core/src/test/java/com/nitorcreations/DomainClassFinderTest.java
+++ drm-core/src/test/java/com/nitorcreations/DomainClassFinderTest.java
@@ -12,21 +12,29 @@ import static org.junit.Assert.assertThat;
 public class DomainClassFinderTest {
 
     @Test
-    public void withsinglePackage() throws Exception {
-        List<Class<?>> classes = DomainClassFinder.findClasses(Arrays.asList("com.nitorcreations.testdomain.person"));
-
+    public void withSinglePackage() throws Exception {
+        List<Class<?>> classes = findClasses("com.nitorcreations.testdomain.person");
         assertThat(classes.size(), is(4));
     }
 
     @Test
     public void withMultiPackages() throws Exception {
-        List<Class<?>> classes = DomainClassFinder.findClasses(Arrays.asList("com.nitorcreations.testdomain.person", "com.nitorcreations.testdomain.another"));
-
+        List<Class<?>> classes = findClasses("com.nitorcreations.testdomain.person", "com.nitorcreations.testdomain.another");
         assertThat(classes.size(), is(5));
+    }
+
+    @Test
+    public void filtersAnonymousClasses() throws Exception {
+        List<Class<?>> classes = findClasses("com.nitorcreations.testdomain.withanonymousclass");
+        assertThat(classes.size(), is(1));
     }
 
     @Test
     public void privateConstructor() {
         CoverageForPrivateConstructor.giveMeCoverage(DomainClassFinder.class);
+    }
+
+    private List<Class<?>> findClasses(String... packages) {
+        return DomainClassFinder.findClasses(Arrays.asList(packages), null);
     }
 }
